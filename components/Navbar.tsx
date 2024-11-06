b29aacb166cb98d2964b63e3d8b2594c432619c1
Elemsimon <elemifesinachisimon@gmail.com>
2024-11-06 09:46:51 +0100

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,49 +1,53 @@
-import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
-import Link from "next/link"
-
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="absolute w-full z-10">
-      <div className="max-w-7xl mx-auto flex justify-between  max-container padding-container py-10">
-      <div className="flex justify-start pt-4">
-        <Link href="/">
-          <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
-        </Link>
-      </div>
-
-      <div className="pt-4">
-        <div className="flex justify-center">
-          <ul className="hidden h-full gap-12 lg:flex">
-              {NAV_LINKS.map((link) => (
-              <Link href={link.href} key={link.key} className="regular-16 text-black flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+      <div className="max-w-7xl mx-auto flex flex-col items-center lg:flex-row lg:justify-between max-container padding-container py-10">
+        
+        {/* Logo Section */}
+        <div className="flex justify-center lg:justify-start mb-4 lg:mb-0">
+          <Link href="/">
+            <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+          </Link>
+        </div>
+        
+        {/* Links Section */}
+        <div className="mb-4 lg:mb-0">
+          <ul className="flex flex-col items-center gap-6 lg:flex-row lg:gap-12">
+            {NAV_LINKS.map((link) => (
+              <li key={link.key}>
+                <Link href={link.href} className="regular-16 text-black cursor-pointer pb-1.5 transition-all hover:font-bold">
                   {link.label}
-              </Link>
-              ))}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
+        
+        {/* Buttons Section */}
+        <div className="flex flex-col items-center gap-4 lg:flex-row lg:gap-4">
+          <button className="text-black">
+            Login
+          </button>
+          <button className="bg-red-600 text-white px-5 rounded-3xl py-3">
+            Sign Up
+          </button>
         </div>
 
-          <div className="flex justify-end gap-4 hidden lg:flex">
-            <button className="">
-              Login
-            </button>
-            <button className="bg-red-600 text-white px-5 rounded-3xl py-3">
-              Sign Up
-            </button>
-          
-        </div>
-      <Image 
-            src="menu.svg"
-            alt="menu"
-            width={32}
-            height={32}
-            className="inline-block cursor-pointer lg:hidden"
+        {/* Mobile Menu Icon */}
+        <Image
+          src="/menu.svg"
+          alt="menu"
+          width={32}
+          height={32}
+          className="inline-block cursor-pointer lg:hidden mt-4"
         />
-    </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
