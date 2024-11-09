cd3c1d0f43f00bbdfd24423b39fa1de4a2e1726e
Elemsimon <elemifesinachisimon@gmail.com>
2024-11-09 21:24:52 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
+import Clients from "@/components/Clients";
+import Dishes from "@/components/Dishes";
+import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Newsletter from "@/components/Newsletter";
+import Order from "@/components/Order";
 
 export default function Home() {
   return (
     <div className="overflow-hidden">
       <Navbar />
       <Hero />
+      <Dishes />
+      <Order />
+      <Clients />
+      <Newsletter />
+      <Footer />
     </div>
   );
 }
